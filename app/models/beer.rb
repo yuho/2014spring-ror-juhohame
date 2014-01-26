204061d35996f29da601ec5e1ace03c67f562a40
Juho Häme <juho.hame@helsinki.fi>
2014-01-26 15:23:21 +0200

    exercise4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,8 +1,14 @@
 class Beer < ActiveRecord::Base
-	belongs_to :brewery
-	has_many :ratings
+  belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    avg=0
+    ratings = Rating.where(beer_id:"#{self.id}")
+    ratings.each do |r|
+      avg += r.score.to_f/ratings.count
+    end
+    return avg
+  end
 end
 
-class Rating < ActiveRecord::Base
-	belongs_to :beer
-end
