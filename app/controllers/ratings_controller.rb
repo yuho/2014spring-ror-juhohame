54abd7875278bf03a36c0ae7fd1e05e90b89fb51
Juho Häme <juho.hame@helsinki.fi>
2014-02-01 20:31:46 +0200

    tht 4

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,15 +10,13 @@ class RatingsController < ApplicationController
 
   def create
     rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-
-    redirect_to ratings_path
+    current_user.ratings << rating
+    redirect_to current_user
   end
 
   def destroy
     rating = Rating.find(params[:id])
     rating.delete
-    redirect_to ratings_path
+    redirect_to :back
   end
 end
