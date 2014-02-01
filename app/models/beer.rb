eaf0f4b2f888e03c72a917919e094f31a2adffbc
Juho Häme <juho.hame@helsinki.fi>
2014-02-01 12:49:33 +0200

    viimeiset viikon 2 mallivastaukset

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,10 +4,6 @@ class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
-  def average_rating
-    Rating.where(beer_id:"#{self.id}").pluck("score").inject(:+).to_f/Rating.where(beer_id:"#{self.id}").count
-  end
-
   def to_s
     "#{name} #{brewery.name}"
   end
