570d96cf06e1f0926a3aa3d1b77e59eea8bf4925
Juho Häme <juho.hame@helsinki.fi>
2014-02-02 21:52:55 +0200

    tht 12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    return nil if session[:user_id].nil?
+    return nil if User.find_by(id: session[:user_id]).nil?
     User.find(session[:user_id])
   end
 
