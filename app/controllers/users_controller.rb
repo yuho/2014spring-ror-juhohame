d793a291d7a8b8500fb9a028c9aac4e15d92fb9e
Juho Häme <juho.hame@helsinki.fi>
2014-02-02 21:58:11 +0200

    tht 13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,10 +54,13 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy if @user == current_user
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+    if @user == current_user
+      @user.destroy
+      @user.ratings.select{ |r| r.user.nil? }.each{ |r| r.delete }
+      respond_to do |format|
+        format.html { redirect_to users_url }
+        format.json { head :no_content }
+      end
     end
   end
 
